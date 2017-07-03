6b8f85d4ad758121d3abd238333521cfb98a38a1
tylerlian <tylerlian@gmail.com>
2017-07-03 13:43:28 -0700

    grading system

---
+++ 6-javascript_continued/3-grading_system.js
@@ -0,0 +1,21 @@
+let my_parameter =process.argv[2];
+if(my_parameter >= 95  ){
+console.log("you recieved an A+")
+} else if (my_parameter >= 90){
+console.log("you recieved an A")
+}
+else if (my_parameter >= 85){
+console.log("you recieved a B+")
+}
+else if (my_parameter >= 80){
+console.log("you recieved a B")
+}
+else if (my_parameter >= 75){
+console.log("you recieved a C+")
+}
+else if (my_parameter >= 70){
+console.log("you recieved a C")
+}
+else if (my_parameter =< 69){
+console.log("you recieved a D")
+}
