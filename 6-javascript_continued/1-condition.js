1ee4bd310c8d7288d3f8d9f54dc37328456dde47
tylerlian <tylerlian@gmail.com>
2017-07-03 13:09:14 -0700

    new condition

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,5 +1,6 @@
 // Store a value passed by the user to the script in the variable my_parameter. Note it takes the second element which corresponds to the first parameter. Third element would correspond to second parameter
 let my_parameter =process.argv[2];
-
+if(my_parameter >= 5){
 // Print the my_parameter variable
-console.log("Here is the first passed parameter:" + my_parameter)
+console.log(" The value is greater than 5")
+}
