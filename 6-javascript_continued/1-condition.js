f4a9bdaa1724d767e9e091faa07c61420844d489
tylerlian <tylerlian@gmail.com>
2017-07-03 13:09:56 -0700

    removed words

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,6 +1,4 @@
-// Store a value passed by the user to the script in the variable my_parameter. Note it takes the second element which corresponds to the first parameter. Third element would correspond to second parameter
 let my_parameter =process.argv[2];
 if(my_parameter >= 5){
-// Print the my_parameter variable
 console.log(" The value is greater than 5")
 }
